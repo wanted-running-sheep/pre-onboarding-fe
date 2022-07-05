76a0b727c568c82a100c7216be2f397652d5de6a
CHOI <tlacjd665@gmail.com>
2022-07-05 15:34:19 +0900

    ✨ feat: 로그인 input 추가

--- src/components/login/Input.jsx
+++ src/components/login/Input.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const LoginInput = ({ placeholder, inputRef }) => {
+const LoginInput = ({
+  name,
+  type,
+  placeholder,
+  inputRef,
+  handleInput,
+  isValidation,
+}) => {
   return (
-    <LoginInputWrapper>
-      <LoginInputContent placeholder={placeholder} ref={inputRef} />
-    </LoginInputWrapper>
+    <Container>
+      <LoginInputContent
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        ref={inputRef}
+        onChange={handleInput}
+        style={{
+          border:
+            isValidation || isValidation === null
+              ? '1px solid #dbdbdb'
+              : '1px solid red',
+        }}
+      />
+    </Container>
   );
 };
 
 export default LoginInput;
 
-const LoginInputWrapper = styled.div`
+const Container = styled.div`
   width: 100%;
   display: flex;
   margin-top: 5px;
@@ -24,5 +43,4 @@ const LoginInputContent = styled.input`
   padding: 10px;
   width: 100%;
   border-radius: 3px;
-  border: 1px solid #dbdbdb;
 `;
