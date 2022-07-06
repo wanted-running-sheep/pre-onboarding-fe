fe29670410a3f20fc0ddeb7c5f74769415676d68
areumsheep <areumsheep@gmail.com>
2022-07-06 22:07:45 +0900

    ♻️ refactor: App에 있는 Router 분리

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Main from './pages/Main';
-import { useAuthState } from './context/AuthContext';
+import Router from './components/Router';
 
 function App() {
-  const { isLoggedIn } = useAuthState();
-
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={isLoggedIn ? <Main /> : <Navigate replace to="/login" />}
-      />
-      <Route
-        path="/login"
-        element={isLoggedIn ? <Navigate replace to="/" /> : <Login />}
-      />
-    </Routes>
-  );
+  return <Router />;
 }
 
 export default App;
